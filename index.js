6c086f162b54234a5769c03689e181e08d267afd
Ikramul-Mahin <ikramul.mahin.islam@gmail.com>
2022-11-09 09:55:29 +0800

    signle data done

--- index.js
+++ index.js
@@ -2,22 +2,52 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+require('dotenv').config()
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 //middle
 app.use(cors())
 app.use(express.json())
 
 //mongo db
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.v0uxjmt.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.v0uxjmt.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+async function run() {
+    try {
+        const tutionCollection = client.db('Tutions').collection('subjects')
+
+        app.get('/subjects', async (req, res) => {
+            const query = {}
+            const cursor = tutionCollection.find(query)
+            const subjects = await cursor.limit(3).toArray()
+            res.send(subjects)
+        })
+        app.get('/subjects/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await tutionCollection.findOne(query)
+            res.send(result)
+        })
+        app.get('/allsubjects', async (req, res) => {
+            const query = {}
+            const cursor = tutionCollection.find(query)
+            const subjects = await cursor.toArray()
+            res.send(subjects)
+        })
+        app.get('/allsubjects/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await tutionCollection.findOne(query)
+            res.send(result)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err))
 
 //test
 app.get('/', (req, res) => {
